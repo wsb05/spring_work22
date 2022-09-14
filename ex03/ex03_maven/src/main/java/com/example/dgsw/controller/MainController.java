5c922a8f42cdea8599d7068678563440d18c558b
우상범 <82503797+wsb05@users.noreply.github.com>
2022-09-14 12:25:11 +0900

    .

--- ex03/ex03_maven/src/main/java/com/example/dgsw/controller/MainController.java
+++ ex03/ex03_maven/src/main/java/com/example/dgsw/controller/MainController.java
@@ -1,13 +1,19 @@
 package com.example.dgsw.controller;
 
+import com.example.dgsw.components.ACompo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
 
+    @Autowired
+    ACompo aCompo;
+
     @GetMapping("")
     public String index(){
+        aCompo.doA();
         return "main";
     }
 
