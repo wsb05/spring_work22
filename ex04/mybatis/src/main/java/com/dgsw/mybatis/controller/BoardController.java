579caa9f5ef175b96c774200e2c46cd935301686
우상범 <82503797+wsb05@users.noreply.github.com>
2022-09-21 14:07:05 +0900

    .

--- ex04/mybatis/src/main/java/com/dgsw/mybatis/controller/BoardController.java
+++ ex04/mybatis/src/main/java/com/dgsw/mybatis/controller/BoardController.java
@@ -1,5 +1,7 @@
 package com.dgsw.mybatis.controller;
 
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,10 +10,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("board")
 public class BoardController {
+
+    @Autowired
+    SqlSessionTemplate sqlSessionTemplate;
+
     // select 해서 테이블 내용 뿌려주는거
     @GetMapping("findall")
     public String findall(Model model){
         System.out.println("findall");
+
+        System.out.println(sqlSessionTemplate.selectList("test.findall"));
 
         model.addAttribute("a", "10");
 
---
+++ ex04/mybatis/src/main/java/com/dgsw/mybatis/dto/Test.java
@@ -0,0 +1,22 @@
+package com.dgsw.mybatis.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+public class Test {
+
+    // Test(String aa, String bb){
+    //      this.aa = aa;
+    //      this.bb = bb;
+    // }
+
+    private String aa;
+    private String bb;
+
+}
