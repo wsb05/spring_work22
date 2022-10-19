46ec801b902262e76f07beabb3b7ee95e85c739f
우상범 <82503797+wsb05@users.noreply.github.com>
2022-10-19 11:20:56 +0900

    .

---
+++ ex05/studyjpa/src/main/java/com/dgsw/studyjpa/StudyjpaApplication.java
@@ -0,0 +1,13 @@
+package com.dgsw.studyjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyjpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyjpaApplication.class, args);
+	}
+
+}
---
+++ ex05/studyjpa/src/test/java/com/dgsw/studyjpa/StudyjpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dgsw.studyjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyjpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
